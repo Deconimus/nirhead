61a2891da32ea9ffffe9397d88305a8e3ef58a13
Deconimus <pascal.sielski@rwth-aachen.de>
2025-03-17 15:09:12 +0100

    update copy_results

--- copy_results.sh
+++ copy_results.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/fakes[0-9][0-9][0-9][0-9][0-9][0-9].png --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/"
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/fakes[0-9][0-9][0-9][0-9][0-9][0-9]_depth.png --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.txt --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.json --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.jsonl --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/evaluations/*.json --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/evaluations/
-curl --upload-file /home/local_admin/work/pascal/gghead/models/gghead/GGHEAD-$1*/checkpoints/checkpoint-2*000.pkl --netrc-file "~/work/pascal/.netrc" smb://192.168.0.30/CCMLOps/2024-11-07-MA_PascalSielski/gghead_results/$2/checkpoints/
+echo -n "Enter password for local_admin@192.168.0.84: "
+read -s password
+
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/fakes[0-9][0-9][0-9][0-9][0-9][0-9].png /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/fakes[0-9][0-9][0-9][0-9][0-9][0-9]_depth.png /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.txt /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.json /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/*.jsonl /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp -r local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/evaluations /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/
+sshpass -p $password scp local_admin@192.168.0.84:~/work/pascal/gghead/models/gghead/GGHEAD-$1*/checkpoints/checkpoint-2*000.pkl /mnt/cccavefs/ccmlops/2024-11-07-MA_PascalSielski/gghead_results/$2/checkpoints/
