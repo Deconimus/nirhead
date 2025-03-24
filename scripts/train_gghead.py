8de278f03660479cd1eead76d9a5d049d1e344d2
Deconimus <pascal.sielski@rwth-aachen.de>
2025-03-24 21:16:10 +0100

    config ada aug

--- scripts/train_gghead.py
+++ scripts/train_gghead.py
@@ -693,7 +693,7 @@ def main(
     # Augmentation.
     if opts.aug != 'noaug':
         c.augment_kwargs = dnnlib.EasyDict(xflip=1, rotate90=1, xint=1, scale=1, rotate=1, aniso=1, xfrac=1,
-                                           brightness=1, contrast=1, lumaflip=1, hue=1, saturation=1)
+                                           brightness=1, contrast=1, lumaflip=0, hue=0, saturation=0)
         if opts.aug == 'ada':
             c.ada_target = opts.target
         if opts.aug == 'fixed':
