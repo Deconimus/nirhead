7f987ccf75b9db463a401a52d3242c741e407d9b
Deconimus <pascal.sielski@rwth-aachen.de>
2025-07-23 18:31:53 +0200

    asd

--- train.sh
+++ train.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 export BW_IMPLEMENTATION=1
-#python scripts/train_gghead.py afhq enir128balanced.zip 1 4 --resume-run gh28_enir_properpose --resume-checkpoint 1000 --kimg 2000 --resolution 128 --plane_resolution 128 --n_uniform_flame_vertices 128 --no_use_gsm_flame_template --use_plane_template --no_use_masks --no_apply_masks --no_blur_masks --image_snapshot_ticks 16 --metrics fid10k --classifier vit16_bp --static_attributes bright_pupil --overwrite-lambda-classifier 0.5
-#python scripts/train_gghead.py afhq enir128balanced.zip 1 4 --kimg 1000 --resolution 128 --plane_resolution 128 --n_uniform_flame_vertices 128 --no_use_gsm_flame_template --use_plane_template --no_use_masks --no_apply_masks --no_blur_masks --image_snapshot_ticks 16 --metrics fid10k --classifier vit16_bp --static_attributes bright_pupil --lambda-classifier 0.5
 
-bash -x synthetic_eo_classifiers_gh75.sh
-bash -x mixed_attr_classifiers_resample.sh
+bash -x synthetic_complement_classifiers.sh
+bash -x synth_aug_ivgaze_classifiers.sh
+bash -x synth_aug_fnir_classifiers.sh
